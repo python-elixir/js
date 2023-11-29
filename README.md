c693ec1ea989562ca873f95e049ddde5cd494fb1
ulugbekpy <ulugbekpycpp@gmail.com>
2023-11-29 09:45:19 +0500

    second project

--- README.md
+++ README.md
@@ -4,6 +4,10 @@
 <a href="https://tbuchburchak.netlify.app/">First project</a>
 ###
 
+###
+<a href="https://zikr-qiling-birodar.netlify.app/">Second project</a>
+###
+
 <div align="left">
   <a href="https://www.linkedin.com/in/ulug-bek-tojimakhammatov-8434b125a/" target="_blank">
     <img src="https://raw.githubusercontent.com/maurodesouza/profile-readme-generator/master/src/assets/icons/social/linkedin/default.svg" width="52" height="40" alt="linkedin logo"  />
--- index.js
+++ index.js
@@ -1,55 +1,11 @@
-let a;
-let b;
-let c;
-document.getElementById("b1").onclick = function(){
-    a = document.getElementById("aSide").value;
-    b = document.getElementById("bSide").value;
-    a = Number(a);
-    b = Number(b);
-    if (a <= 0 || b <= 0){
-        document.getElementById("resultC").innerHTML = "O'yin qilmang, ukaxon!"
-    }else{
-        c = Math.sqrt(Math.pow(a,2) + Math.pow(b,2));
-        document.getElementById("resultC").innerHTML = "c tomon: " + c
-    }
+let count = 0;
+
+document.getElementById("counting").onclick = function (){
+    count += 1
+    document.getElementById("sanoq").innerHTML = count;
 }
 
-document.getElementById("yuza1").onclick = function(){
-    a = document.getElementById("aSide").value;
-    b = document.getElementById("bSide").value;
-    a = Number(a);
-    b = Number(b);
-    if (a <= 0 || b <= 0){
-        document.getElementById("resultC").innerHTML = "O'yin qilmang, ukaxon!"
-    }else{
-        c = a*b/2
-        document.getElementById("resultC").innerHTML = "Yuza: " + c
-    }
-}
-
-document.getElementById("b2").onclick = function(){
-    a = document.getElementById("aside").value;
-    c = document.getElementById("cside").value;
-    a = Number(a);
-    c = Number(c);
-    if (a >= c || a <= 0 || c <= 0){
-        document.getElementById("resultb").innerHTML = "O'yin qilmang, ukaxon!"
-    }else{
-        b = Math.sqrt(Math.pow(c,2) - Math.pow(a,2));
-        document.getElementById("resultb").innerHTML = "b tomon: " + b
-    }
-}
-
-document.getElementById("yuza2").onclick = function(){
-    a = document.getElementById("aside").value;
-    c = document.getElementById("cside").value;
-    a = Number(a);
-    c = Number(c);
-    if (a >= c || a <= 0 || c <= 0){
-        document.getElementById("resultb").innerHTML = "O'yin qilmang, ukaxon!"
-    }else{
-        b = Math.sqrt(Math.pow(c,2) - Math.pow(a,2));
-        b = b * a /2
-        document.getElementById("resultb").innerHTML = "Yuza: " + b
-    }
+document.getElementById("reset").onclick = function (){
+    count = 0;
+    document.getElementById("sanoq").innerHTML = count;
 }
